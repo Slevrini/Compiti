6578a7d1c00f7ebef18e54c72134866eec8695fd
Slevrini <59537815+Slevrini@users.noreply.github.com>
2021-03-26 20:27:55 +0100

    inserito l'attacco

---
+++ battaglia-navale/hit.js
@@ -0,0 +1,61 @@
+const express = require("express")
+const app = new express()
+const fetch = require("node-fetch")
+
+const port = 8000
+
+const team = "username"
+const password = "password"
+
+const login = () => {
+  fetch("http://localhost:8080/signup", {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: "POST",
+    body: JSON.stringify({ team, password })
+  })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
+}
+const hit = (cell) => {
+  fetch("http://localhost:8080/fire", {
+    headers: {
+      "Content-Type": "application/json"
+    },
+    method: "POST",
+    body: JSON.stringify({
+      team, password,
+      x: cell.x,
+      y: cell.y
+    })
+  })
+    .then(res => res.json())
+    .then(data => console.log(data))
+    .catch(err => console.error(err))
+}
+
+const getField = () => {
+    fetch("http://localhost:8080/?format=json")
+      .then(res => res.json())
+      .then(data => {
+        const tempField = data.visibleField.map(row => row.filter(cell => !cell.hit))
+        const endGame = !tempField.every(row => row.every(cell => !cell))
+        if (endGame) {
+          const field = tempField.filter(row => row.length > 0)
+          const yrandom = Math.floor(Math.random() * field.length)
+          const xrandom = Math.floor(Math.random() * field[yrandom].length)
+          const cell = field[yrandom][xrandom]
+          hit(cell)
+        } else {
+          console.log("Tutte le caselle sono state colpite")
+        }
+      })
+      .catch(err => console.error(err))
+  }
+  
+  login()
+  setInterval(getField, 1001)
+
+  app.listen(port, () => console.log("Listening on port", port))
